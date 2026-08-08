e211508aeb8096bae133f6b1b859df69f88d5940
Kpemini Delali Beatrice <kpeminibeatrice18@gmail.com>
2026-08-08 15:47:59 +0000

    modified to add fetching weather data logic  based on one's location

--- weather_app/scripts/api.js
+++ weather_app/scripts/api.js
@@ -1,5 +1,6 @@
 const apiKey = "d3de143a3080e43f543f4ceedcb1d34f";
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const coordsBaseUrl = "https://api.openweathermap.org/data/2.5/weather";
 
 export async function getWeatherData(city) {
     try {
@@ -14,5 +15,21 @@ export async function getWeatherData(city) {
 
     } catch(error) {
         console.log(`Something went wrong: ${error}`);
+    }
+}
+
+
+export async function getWeatherDataByCoords (lat, lon) {
+    try {
+        const response = await fetch(`${coordsBaseUrl}?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+
+        if (!response.ok) {
+            throw new Error("Location weather not found.");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        console.log(`Something went wrong: ${error}`)
     }
 }
